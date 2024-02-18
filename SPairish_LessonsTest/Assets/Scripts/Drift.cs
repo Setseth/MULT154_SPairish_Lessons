6fba67daa92221718af046e1c2d6f03a9431017f
Setseth <48038201+Setseth@users.noreply.github.com>
2024-02-18 11:44:57 -0800

    Lesson1

---
+++ SPairish_LessonsTest/Assets/Scripts/Drift.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Security.AccessControl;
+using UnityEngine;
+
+public class Drift : MonoBehaviour
+{
+    public float speed = 5.0f;
+    public enum DriftDirection
+    {
+        LEFT = -1,
+        RIGHT = 1
+    }
+    public DriftDirection driftDirecion = DriftDirection.LEFT;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        switch(driftDirecion)
+        {
+            case DriftDirection.LEFT:
+                transform.Translate(Vector3.left * Time.deltaTime * speed);
+                break;
+            case DriftDirection.RIGHT:
+                transform.Translate(Vector3.right * Time.deltaTime * speed);
+                break;
+
+        }
+
+        if(transform.position.x < -80 || transform.position.x > 80)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.CompareTag("Player"))
+        {
+            GameObject child = collision.gameObject;
+            child.transform.SetParent(gameObject.transform);
+        }
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        if(collision.gameObject.CompareTag("Player"))
+        {
+            GameObject child = collision.gameObject;
+            child.transform.SetParent(null);
+        }
+    }
+}
